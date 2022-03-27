f9c0ac14030e5db0ab6e5d11f7dd5b34fd9a45fb
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-27 22:52:06 +0700

    add: list using map key

--- react-fundamental/hello-world/src/components/NameList.js
+++ react-fundamental/hello-world/src/components/NameList.js
@@ -1,14 +1,32 @@
 import React from "react";
 
 function NameList() {
-  const names = ["eren", "mikasa", "armin"];
-  return (
-    <div>
-      {
-        names.map(name => <h1 key={name}>{name}</h1>)
-      }
-    </div>
-  );
+  const persons = [
+    {
+      id: 1,
+      name: "Eren",
+      age: 22,
+      skill: "React",
+    },
+    {
+      id: 2,
+      name: "Mikasa",
+      age: 20,
+      skill: "IOS",
+    },
+    {
+      id: 3,
+      name: "Armin",
+      age: 22,
+      skill: "Kotlin",
+    },
+  ];
+  const personList = persons.map((person) => (
+    <h1>
+      I am {person.name}, {person.age} years old and my skill is {person.skill}
+    </h1>
+  ));
+  return <div>{personList}</div>;
 }
 
 export default NameList;
