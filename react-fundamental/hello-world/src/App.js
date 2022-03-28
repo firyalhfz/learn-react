69cea6e50eef7d9277c2fe350bb864017f0a9cc3
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-28 14:40:31 +0700

    add: jsx

--- react-fundamental/hello-world/src/App.js
+++ react-fundamental/hello-world/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import NameList from "./components/NameList";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
       <Welcome> </Welcome>
+      <Hello />
       {/* <NameList></NameList> */}
     </div>
   );
