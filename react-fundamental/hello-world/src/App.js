026d0d50607d03d21494fb831c639d0f43cb82a5
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-30 15:09:57 +0700

    add: setState

--- react-fundamental/hello-world/src/App.js
+++ react-fundamental/hello-world/src/App.js
@@ -4,10 +4,12 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
+      <Counter />
       <Message />
       <Greet name="firyal" heroName="armin">
         {' '}
