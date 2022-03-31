161cc9ca0a367478748d070c4d87b42797b17dae
firyalhuwaida <iyaalfiryal@gmail.com>
2022-03-31 16:35:44 +0700

    add: success mapping yey

--- react-fundamental/mapping-data/src/App.js
+++ react-fundamental/mapping-data/src/App.js
@@ -2,12 +2,14 @@ import { Box } from '@mui/material'
 import './App.css'
 import Card from './components/Card'
 import CardItem from './components/CardItem'
+import NewCard from './components/NewCard'
 
 function App() {
   return (
     <div className="App">
       <Box sx={{ maxWidth: { xs: 450, sm: 450 } }}>
-        <CardItem sx={{ maxWidth: { xs: 450, sm: 450 } }} />
+        {/* <CardItem sx={{ maxWidth: { xs: 450, sm: 450 } }} /> */}
+        <NewCard />
       </Box>
     </div>
   )
---
+++ react-fundamental/mapping-data/src/components/NewCardItem.js
@@ -0,0 +1,42 @@
+import { CardMedia, Typography } from '@mui/material'
+import React from 'react'
+import Paper from '@mui/material/Paper'
+import Stack from '@mui/material/Stack'
+import { styled } from '@mui/material/styles'
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}))
+
+const Img = styled('img')({
+  margin: 'auto',
+  display: 'block',
+  maxWidth: '100%',
+  maxHeight: '100%',
+})
+
+const NewCardItem = props => {
+  console.log(props.dataCard)
+  return (
+    <Stack spacing={2}>
+      {props.dataCard.map(card => {
+        return (
+          <Item>
+            <Typography key={card.id}>{card.title}</Typography>
+            <Typography key={card.id}>{card.subtitle}</Typography>
+            <Typography key={card.id}>{card.price}</Typography>
+            <CardMedia key={card.id}>
+              <Img src={card.img} />
+            </CardMedia>
+          </Item>
+        )
+      })}
+    </Stack>
+  )
+}
+
+export default NewCardItem
