30ac74d6488e3a1cde77de161581b5e057fc6b7a
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-01 20:28:51 +0700

    add: total

--- cart-app/src/components/Basket.js
+++ cart-app/src/components/Basket.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 function Basket(props) {
   const { cartItems, onAdd, onRemove } = props
+  const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0)
+  const taxPrice = itemsPrice * 0.14;
+  const shippingPrice = itemsPrice > 2000 ? 0 : 50;
+  const totalPrice = itemsPrice + taxPrice + shippingPrice
+
   // aside is html tag to show side bar
   return (
     <aside className="block col-1">
@@ -23,6 +28,30 @@ function Basket(props) {
           </div>
         </div>
       ))}
+      {/* todo3 */}
+      {cartItems.length !== 0 && (
+        <>
+          <hr></hr>
+          <div className="row">
+            <div className="col-2">Item Price</div>
+            <div className="col-1 text-right">{itemsPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">Tax Price</div>
+            <div className="col-1 text-right">{taxPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">Shipping price</div>
+            <div className="col-1 text-right">{shippingPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">
+              <strong>Total Price</strong>
+            </div>
+            <div className="col-1 text-right"><strong>{totalPrice.toFixed(2)}</strong></div>
+          </div>
+        </>
+      )}
     </aside>
   )
 }
