e095c9ac9a91afb2c3c03460b57dad42e7bfec7a
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-07 17:21:01 +0700

    fix error github

--- react-fundamental/hello-world/react-fundamental/hello-world/src/components2/Welcome.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Welcome extends Component {
-  render() {
-    return <h1>Welcome {this.props.name} a.k.a {this.props.heroName}</h1>
-  }
-}
-
-export default Welcome
--- react-fundamental/hello-world/src/App.js
+++ react-fundamental/hello-world/src/App.js
@@ -1,15 +1,15 @@
 import './App.css'
-import NameList from './components/NameList'
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/Message'
-import Counter from './components/Counter'
+// import NameList from './components2/NameList'
+// import Greet from './components2/Greet'
+// import Welcome from './components2/Welcome'
+// import Hello from './components2/Hello'
+// import Message from './components2/Message'
+// import Counter from './components2/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      {/* <Counter />
       <Message />
       <Greet name="firyal" heroName="armin">
         {' '}
@@ -21,7 +21,7 @@ function App() {
       <Greet name="salwa" heroName="mikasa" />
       <Welcome name="firyal" heroName="armin" />
       <Welcome name="ilyas" heroName="eren" />
-      <Welcome name="salwa" heroName="mikasa" />
+      <Welcome name="salwa" heroName="mikasa" /> */}
       {/* <Hello /> */}
       {/* <NameList></NameList> */}
     </div>
