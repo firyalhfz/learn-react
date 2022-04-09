c8836aeb2846e87617114ceb6ae758f779d008d0
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-09 14:39:31 +0700

    add: dynamic value

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = "welcome to my app"
+  const age = 10;
+  const link = "http://github.com/firyaldev"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}</h1>
+      <h1>{age * 2}</h1>
+      <a href={link}>Go to github firyal </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
