ae239580e30303578d6e9068067ca8d3dd7b7317
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-09 15:05:30 +0700

    add: useState

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,12 +1,20 @@
-import Header from "./components/Header";
+import Header from './components/Header'
+import { useState } from 'react'
 
 function App() {
-  const title = "welcome to my app"
-  const age = 10;
-  const link = "http://github.com/firyaldev"
+  // const title = 'welcome to my app'
+  // const age = 10
+  const link = 'http://github.com/firyaldev'
 
   const clickMe = (name) => {
-    console.log('clicked: ' + name);
+    console.log('clicked: ' + name)
+  }
+
+  const [title, setTitle] = useState('')
+  const [age, setAge] = useState(20)
+  const changeTitle = () => {
+    setTitle('title changed')
+    setAge(40)
   }
 
   return (
@@ -15,7 +23,8 @@ function App() {
       <h1>{age * 2}</h1>
       <a href={link}>Go to github firyal </a>
       <Header />
-      <button onClick={() => clickMe("firyal")}>Click me </button>
+      <button onClick={() => clickMe('firyal')}>Click me </button>
+      <button onClick={changeTitle}>Change Title </button>
     </div>
   )
 }
