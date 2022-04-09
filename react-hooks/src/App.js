8c05d7254c45d0bf70e912145d42e303954c5e42
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-09 14:50:45 +0700

    add: click event

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -5,12 +5,17 @@ function App() {
   const age = 10;
   const link = "http://github.com/firyaldev"
 
+  const clickMe = (name) => {
+    console.log('clicked: ' + name);
+  }
+
   return (
     <div className="App">
       <h1>{title}</h1>
       <h1>{age * 2}</h1>
       <a href={link}>Go to github firyal </a>
       <Header />
+      <button onClick={() => clickMe("firyal")}>Click me </button>
     </div>
   )
 }
