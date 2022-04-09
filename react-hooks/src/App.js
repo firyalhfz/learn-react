1d1f36447a527b6f9ac2515860d8ab461faeb7cb
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-09 15:47:47 +0700

    add: looping list

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -4,29 +4,45 @@ import { useState } from 'react'
 function App() {
   // const title = 'welcome to my app'
   // const age = 10
-  const link = 'http://github.com/firyaldev'
+  // const link = 'http://github.com/firyaldev'
 
-  const clickMe = (name) => {
-    console.log('clicked: ' + name)
-  }
+  // const clickMe = name => {
+  //   console.log('clicked: ' + name)
+  // }
 
-  const [title, setTitle] = useState('')
-  const [age, setAge] = useState(20)
-  const changeTitle = () => {
-    setTitle('title changed')
-    setAge(40)
-  }
+  // const [title, setTitle] = useState('')
+  // const [age, setAge] = useState(20)
+  const [products, setProducts] = useState([
+    { id: 1, title: 'Product 1', price: 900 },
+    { id: 2, title: 'Product 2', price: 500 },
+    { id: 3, title: 'Product 3', price: 700 },
+    { id: 4, title: 'Product 4', price: 300 },
+    { id: 5, title: 'Product 5', price: 800 },
+  ])
+
+  // const changeTitle = () => {
+  //   setTitle('title changed')
+  //   setAge(40)
+  // }
 
   return (
     <div className="App">
-      <h1>{title}</h1>
+      {/* <h1>{title}</h1>
       <h1>{age * 2}</h1>
       <a href={link}>Go to github firyal </a>
       <Header />
       <button onClick={() => clickMe('firyal')}>Click me </button>
-      <button onClick={changeTitle}>Change Title </button>
+      <button onClick={changeTitle}>Change Title </button> */}
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.title} - {product.price}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
 
 export default App
+
